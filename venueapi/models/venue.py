b3902a816161fc9937a7f92e78a319fbe4f66a82
Nicholas Williams <nicholaswilliams910@gmail.com>
2023-12-11 11:00:35 -0600

    created venues.json fixture

--- venueapi/models/venue.py
+++ venueapi/models/venue.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 class Venue(models.Model):
-    venue_image_url = models.URLField(max_length=500, null=True, blank=True)
+    venue_outside_image_url = models.URLField(max_length=500, null=True, blank=True)
+    venue_inside_image_url = models.URLField(max_length=500, null=True, blank=True)
     name = models.CharField(max_length=150, null=True, blank=True)
     address = models.CharField(max_length=300, null=True, blank=True)
     capacity = models.IntegerField(null=True, blank=True)
